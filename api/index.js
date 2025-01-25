0e15f829907f87d9b9ebcbaab065ac6ebc432a93
asifali-7890 <78083087+asifali-7890@users.noreply.github.com>
2025-01-25 23:39:59 +0530

    Add dotenv support, refactor MongoDB connection, and update Profile component text

--- api/index.js
+++ api/index.js
@@ -1,14 +1,29 @@
-const express = require('express');
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-const PORT = 3000;
+// MongoDB connection string
+const dbURI = process.env.MONGO_URL;
+
+// Connect to MongoDB
+mongoose.connect(dbURI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+        // Start the server only after successful connection
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error('Failed to connect to MongoDB', err);
+    });
 
 // Define a route for the root URL
 app.get('/', (req, res) => {
     res.send({ name: 'Hello, World!' });
-});
-
-app.listen(PORT, (req, res) => {
-    console.log(`Server is running on port ${PORT}`);
 });
--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,7 +1,7 @@
 const Profile = () => {
   return (
     <div>
-      Profile
+      Visit our profile
     </div>
   )
 }
