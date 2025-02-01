32ca108eb3bfbfe1a782ae5badf4900b756cd5f6
asifali-7890 <78083087+asifali-7890@users.noreply.github.com>
2025-02-01 19:42:52 +0530

    Add search functionality to Header component; implement search term state and navigation

--- client/src/Pages/Header.jsx
+++ client/src/Pages/Header.jsx
@@ -1,10 +1,30 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { FaSearch } from "react-icons/fa";
 import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 
 
 export default function Header() {
     const currentUser = useSelector((state) => state.user.currentUser);
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const navigate = useNavigate();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams(window.location.search);
+        urlParams.set('searchTerm', searchTerm);
+        const searchQuery = urlParams.toString();
+        navigate(`/search?${searchQuery}`);
+    };
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get('searchTerm');
+        if (searchTermFromUrl) {
+            setSearchTerm(searchTermFromUrl);
+        }
+    }, [location.search]);
+
 
     return (
         <header className='bg-slate-200 shadow-md'>
@@ -20,8 +40,14 @@ export default function Header() {
                     </Link>
                 </h1>
                 <form className='rounded-full flex items-center gap-2 p-3 bg-slate-100'>
-                    <input type="search" placeholder="Search..." className='bg-transparent outline-none w-24 sm:w-64' />
-                    <FaSearch className='text-slate-600' />
+                    <input
+                        type="search"
+                        placeholder="Search..."
+                        className='bg-transparent outline-none w-24 sm:w-64'
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                    />
+                    <FaSearch onClick={handleSubmit} className='text-slate-600' />
                 </form>
                 <div className='flex gap-2'>
                     <ul className='flex gap-2'>
